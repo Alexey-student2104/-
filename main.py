6401687a695a52a9e6f24da7d0fc5d157675d645
Alexey-student2104 <mamalex2147@mail.ru>
2025-09-19 21:58:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ class Solution:
     def two_sum(self, nums, target):
         for num in nums:
             if not isinstance(num, int):
-                raise TypeError(f"Все элементы массива должны быть целыми числами. Найден элемент типа {type(num).__name__}: {num}")
-        
+                raise TypeError(
+                    f"Все элементы массива должны быть целыми числами. Найден элемент типа {type(num).__name__}: {num}")
+
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
@@ -11,9 +12,10 @@ class Solution:
                     return [i, j]
         return []
 
+
 my_solution = Solution()
 
 # Тестовые случаи
-print(my_solution.two_sum([2, 7, 11, 15], 9))  
+print(my_solution.two_sum([2, 7, 11, 15], 9))
 print(my_solution.two_sum([3, 2, 4], 6))
 print(my_solution.two_sum([3, 2, 4], 6))
